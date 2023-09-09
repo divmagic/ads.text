7ccd71c8e05644b74f98d2010a8c0be5e33dbe17
divmagic <126518599+divmagic@users.noreply.github.com>
2023-09-09 23:28:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://one.akw.cam/", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var sourceCode = xhr.responseText;
-    console.log(sourceCode);
-  } else if (xhr.readyState === 4) {
-    console.error("Error:", xhr.status);
-  }
-};
-xhr.send();
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Display HTML Source Code</title>
+</head>
+<body>
+  <script>
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://one.akw.cam/", true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var sourceCode = xhr.responseText;
+        console.log(sourceCode);
+      } else if (xhr.readyState === 4) {
+        console.error("Error:", xhr.status);
+      }
+    };
+    xhr.send();
+  </script>
+</body>
+</html>
