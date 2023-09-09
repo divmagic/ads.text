59b0e04856150b6ac11556e082d5290a924cfd16
divmagic <126518599+divmagic@users.noreply.github.com>
2023-09-09 23:25:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-window.location.href = "view-source:https://one.akw.cam/";
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://one.akw.cam/", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var sourceCode = xhr.responseText;
+    console.log(sourceCode);
+  } else if (xhr.readyState === 4) {
+    console.error("Error:", xhr.status);
+  }
+};
+xhr.send();
