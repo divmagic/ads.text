6a6687491358491d8c5077401134a05ea1b54d3e
divmagic <126518599+divmagic@users.noreply.github.com>
2023-09-09 23:24:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-fetch("https://one.akw.cam/")
-  .then(response => response.text())
-  .then(data => {
-    var htmlElement = document.createElement('html');
-    htmlElement.innerHTML = data;
-    document.body.appendChild(htmlElement);
-  })
-  .catch(error => console.error(error));
+window.location.href = "view-source:https://one.akw.cam/";
